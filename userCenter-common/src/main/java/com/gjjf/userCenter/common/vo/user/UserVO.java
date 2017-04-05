0e9db46cac2d464d2c72ae9ef64f893780e7a189
henser123 <henser123@163.com>
2017-04-05 19:47:06 +0800

    用户中心common类

---
+++ userCenter-common/src/main/java/com/gjjf/userCenter/common/vo/user/UserVO.java
@@ -0,0 +1,55 @@
+package com.gjjf.userCenter.common.vo.user;
+
+public class UserVO {
+	
+	private String user_phone;
+	
+	private String user_pwd;
+	
+	private String user_name;
+	
+	private String email;
+	
+	private String uuid;
+
+	public String getUser_phone() {
+		return user_phone;
+	}
+
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
+	}
+
+	public String getUser_pwd() {
+		return user_pwd;
+	}
+
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	
+}
