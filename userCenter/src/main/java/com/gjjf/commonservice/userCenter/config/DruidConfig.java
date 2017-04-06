6ae96382c520bc8fc2edd43945d8ddac1ae76ea3
henser123 <henser123@163.com>
2017-04-06 19:02:57 +0800

    提交；

--- userCenter/src/main/java/com/gjjf/commonservice/userCenter/config/DruidConfig.java
+++ userCenter/src/main/java/com/gjjf/commonservice/userCenter/config/DruidConfig.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -19,6 +20,7 @@ import com.alibaba.druid.pool.DruidDataSource;
  *
  */
 @Configuration
+@ConfigurationProperties(prefix = DataSourceProperties.PREFIX, ignoreUnknownFields = false)
 public class DruidConfig {
 	
 	private Logger logger = LoggerFactory.getLogger(getClass());
