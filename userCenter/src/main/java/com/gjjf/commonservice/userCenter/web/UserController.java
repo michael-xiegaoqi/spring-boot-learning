cc08866956db904db4e75dfd140b84584dd026f6
henser123 <henser123@163.com>
2017-04-10 15:12:51 +0800

    添加swagger2；

--- userCenter/src/main/java/com/gjjf/commonservice/userCenter/web/UserController.java
+++ userCenter/src/main/java/com/gjjf/commonservice/userCenter/web/UserController.java
@@ -1,5 +1,7 @@
 package com.gjjf.commonservice.userCenter.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,17 +12,27 @@ import com.gjjf.commonservice.userCenter.model.User;
 import com.gjjf.commonservice.userCenter.service.UserService;
 import com.gjjf.framework.rest.Result;
 import com.gjjf.framework.rest.ResultCode;
+import com.gjjf.framework.util.IntUtil;
 import com.gjjf.userCenter.common.vo.user.UserVO;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api("用户中心相关api")
 public class UserController {
     
+	private static Logger logger = LoggerFactory.getLogger(UserController.class);
+	
     @Autowired
     private UserService userService;
     
     @Autowired
     private UserSender userSender;
     
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "vo", value = "用户详细实体user", required = true, dataType = "UserVO")
     @RequestMapping(value = "/user/insert", method = RequestMethod.POST)
     public Result insert(UserVO vo) {
         
@@ -32,10 +44,19 @@ public class UserController {
     	
     }
     
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(paramType="query", name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value = "/user/get", method = RequestMethod.GET)
     public Result get(Long id) {
         
     	Result result = new Result();
+    	
+    	logger.info("传入参数ID ：{}",id);
+    	
+    	if(IntUtil.isZero(id)){
+    		result.setMessage("传入参数ID为空");
+    		return result;
+    	}
     	
     	User user = userService.get(id);
     	
@@ -56,6 +77,7 @@ public class UserController {
     	
     }
     
+    @ApiOperation(value="消息发送", notes="消息发送测试方法")
     @RequestMapping(value = "/user/send", method = RequestMethod.GET)
     public Result send(UserVO vo) {
         
